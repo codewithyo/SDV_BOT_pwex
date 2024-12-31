4e4149865021900449de405bf4ce6aed5718bfc6
Unknownkiller7777 <126313471+Unknownkil@users.noreply.github.com>
2024-12-31 21:19:11 +0530

    Update pw_handler.py

--- pw_handler.py
+++ pw_handler.py
@@ -183,7 +183,7 @@ async def handle_subject_ids(update: Update, context: ContextTypes.DEFAULT_TYPE)
             # Send the file to the user
             try:
                 with open(file_path, 'rb') as file:
-                    await update.message.reply_document(file, caption=f"Contents for {subject_name}.")
+                    await update.message.reply_document(file, caption=f"Contents for 𝑩𝒂𝒕𝒄𝒉 𝑵𝒂𝒎𝒆😶‍🌫️: ```{name}```.")
             except Exception as e:
                 await update.message.reply_text(f"Error sending file to user: {e}")
                 continue
