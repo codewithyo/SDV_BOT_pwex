20052d5905ddc83a86ea83330376a2f6f62a4004
Unknownkiller7777 <126313471+Unknownkil@users.noreply.github.com>
2025-01-11 12:21:54 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import logging
 from flask import Flask
 from telegram.ext import ApplicationBuilder, CommandHandler
-from pw_handler import pw_handler
-from ak_handler import ak_handler
-from iit_handler import iit_handler  # Import the new handler
+from pw_handler import pw_handler  # Import the new handler
 from config import BOT_TOKEN
 
 logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
@@ -30,7 +28,5 @@ if __name__ == "__main__":
         await update.message.reply_text("𝐻𝑒𝑙𝑙𝑜 𝑢𝑠𝑒𝑟  😉 𝐼'𝑚 𝐴 𝑆𝑖𝑚𝑝𝑙𝑒 𝐵𝑎𝑡𝑐ℎ 𝑡𝑜 𝑇𝑥𝑇 𝑒𝑥𝑡𝑟𝑎𝑐𝑡𝑜𝑟 𝐵𝑜𝑡\n\n𝑈𝑠𝑒 𝑡ℎ𝑒𝑠𝑒 𝑐𝑜𝑚𝑚𝑎𝑛𝑑𝑠:\n🫠 /pw - 𝑓𝑜𝑟 𝑃𝑊 𝑐𝑜𝑛𝑡𝑒𝑛𝑡\n🫠 /iit - 𝑓𝑜𝑟 𝐼𝐼𝑇 𝑆𝑐ℎ𝑜𝑜𝑙 𝑐𝑜𝑛𝑡𝑒𝑛𝑡\n🫠 /ak - 𝑓𝑜𝑟  𝑨𝒑𝒏𝒊 𝒌𝒂𝒌𝒔𝒉𝒂 𝑐𝑜𝑛𝑡𝑒𝑛𝑡")
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(pw_handler)
-    application.add_handler(ak_handler) 
-    application.add_handler(iit_handler)  # Add the new IIT handler
+    application.add_handler(pw_handler) # Add the new IIT handler
     application.run_polling()
